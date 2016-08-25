272e27e41c3d14449911614324286c4617fbf4f0
AlfredoSM <ades_alfred@hotmail.com>
2016-08-25 15:41:05 -0500

    Update ejemplo-extrusion.js

--- rv/ejemplo-extrusion.js
+++ rv/ejemplo-extrusion.js
@@ -5,7 +5,7 @@ figura.lineTo(10, 40);
 figura.lineTo(40, 40);
 figura.lineTo(10, 10);
 
-var forma = new Three.ExtrudeGeometry(figura, {amount: 10} );
+var forma = new THREE.ExtrudeGeometry(figura, {amount: 10} );
 var material = new THREE.Mesh.NormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/4 );
