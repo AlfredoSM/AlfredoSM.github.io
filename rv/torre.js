bd704544b6a92dff1e5d1150544a98bddd573a4a
AlfredoSM <ades_alfred@hotmail.com>
2016-08-30 15:32:43 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,28 +1,28 @@
 var figura = new THREE.Shape();
 
 figura.moveTo(0, 0);
-figura.lineTo(5, 0);
-figura.lineTo(5, 5);
-figura.lineTo(0, 5);
+figura.lineTo(2, 0);
+figura.lineTo(2, 2);
+figura.lineTo(0, 2);
 figura.lineTo(0, 0);
 
 figura.moveTo(0, 10);
-figura.lineTo(0, 15);
-figura.lineTo(5, 15);
-figura.lineTo(5, 10);
+figura.lineTo(0, 12);
+figura.lineTo(2, 12);
+figura.lineTo(2, 10);
 figura.lineTo(0, 10);
 
 figura.moveTo(10, 10);
-figura.lineTo(10, 15);
-figura.lineTo(15, 15);
-figura.lineTo(15, 10);
+figura.lineTo(10, 12);
+figura.lineTo(12, 12);
+figura.lineTo(12, 10);
 figura.lineTo(10, 10);
 
 figura.moveTo(10, 0);
-figura.lineTo(10, 5);
-figura.lineTo(15, 5);
-figura.lineTo(15, 0);
-figura.lineTo(10, 0);
+figura.lineTo(10, 2);
+figura.lineTo(12, 2);
+figura.lineTo(12, 0);
+figura.lineTo(12, 0);
 
 var forma = new THREE.ExtrudeGeometry(figura, {amount: 1} );
 var material = new THREE.MeshNormalMaterial();
