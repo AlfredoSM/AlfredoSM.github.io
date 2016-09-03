949e4c92db544108ac9a26f923f4aa7b44678c0a
AlfredoSM <ades_alfred@hotmail.com>
2016-09-02 21:52:51 -0500

    Create estrella.js

---
+++ rv/estrella.js
@@ -0,0 +1,26 @@
+figura.moveTo(10, 10);
+figura.lineTo(40, 10);
+figura.lineTo(50, -10);
+figura.lineTo(60, 10);
+figura.lineTo(90, 10);
+figura.lineTo(80, 30);
+figura.lineTo(90, 50);
+figura.lineTo(60, 50);
+figura.lineTo(50, 70);
+figura.lineTo(40, 50);
+figura.lineTo(10, 50);
+figura.lineTo(20, 30);
+figura.lineTo(10, 10);
+var forma = new THREE.ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
