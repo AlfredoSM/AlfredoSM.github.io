43bb057ebf99cd24734966dcc18d33b803b49742
AlfredoSM <ades_alfred@hotmail.com>
2016-09-13 15:36:23 -0500

    Update sombras.js

--- rv/sombras.js
+++ rv/sombras.js
@@ -1,6 +1,9 @@
 var iluminacion = new THREE.PointLight(0xFFFFFF);
 iluminacion.position.y=2;
-
+var forma = new THREE.SphereGeometry( 1 );
+var material = new THREE.MeshLambertMaterial({color: 0X00CC00});
+var malla = new THREE.Mesh(forma, material);
+malla.position,y = 2;
 var base = new THREE.Mesh( new THREE.BoxGeometry(5, .1, 5), new THREE.MeshLambertMaterial({color: 0xFFFFFF}));
 var escena = new THREE.Scene();
 escena.add(malla);
