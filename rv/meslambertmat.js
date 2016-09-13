b19d31b7b67fbaa784f2ebde38c965a7e64d3107
AlfredoSM <ades_alfred@hotmail.com>
2016-09-13 14:59:15 -0500

    Create meslambertmat.js

---
+++ rv/meslambertmat.js
@@ -0,0 +1,13 @@
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: '#00cc00'});
+var malla = new THREE.Mesh( forma, material);
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( 600, 600 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
