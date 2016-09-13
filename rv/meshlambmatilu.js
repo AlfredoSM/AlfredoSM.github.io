5651cae0fcb952ce7de3caebb1e7840aa9e658c1
AlfredoSM <ades_alfred@hotmail.com>
2016-09-13 15:08:51 -0500

    Create meshlambmatilu.js

---
+++ rv/meshlambmatilu.js
@@ -0,0 +1,18 @@
+var luzPuntual = new TRHEE.PointLight(0xFFFFFF);
+luzPuntual.position.x = 10;
+luzPuntual.position.y =10;
+luzPuntual.position.z =10;
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial({color: '#00cc00'});
+var malla = new THREE.Mesh( forma, material);
+var escena = new THREE.Scene();
+escena.add(malla);
+escena.add(luzPuntual);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( 600, 600 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
