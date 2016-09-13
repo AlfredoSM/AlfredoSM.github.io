f3ab3ddd8819f78fc3951f60f62a8a92eec2c5fb
AlfredoSM <ades_alfred@hotmail.com>
2016-09-13 14:48:21 -0500

    Create ejemmatbasiccolor.js

---
+++ ejemmatbasiccolor.js
@@ -0,0 +1,14 @@
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshBasicMaterial( {color: 0x00ff00 });
+var malla = new THREE.Mesh( forma, material );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( 600, 600 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
