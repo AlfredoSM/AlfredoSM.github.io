655fc1a3e813aca2ec7d51c566759885eb361e84
AlfredoSM <ades_alfred@hotmail.com>
2016-09-20 14:55:48 -0500

    Update tableroluces.js

--- rv/tableroluces.js
+++ rv/tableroluces.js
@@ -1,5 +1,5 @@
 var luz1 = new THREE.PointLight( 0xFFFFFF );
-luz1.position.y = 1000;
+luz1.position.y =- 1000;
 luz1.position.x = 900;
 luz1.position.z = 900;
 var material1 = new THREE.MeshLambertMaterial( { color: 0xbbbbbb } );
