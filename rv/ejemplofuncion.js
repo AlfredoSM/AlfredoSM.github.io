86ef2a87c2497da4a84a997feab48ddb533ef355
AlfredoSM <ades_alfred@hotmail.com>
2016-09-22 15:46:17 -0500

    Update ejemplofuncion.js

--- rv/ejemplofuncion.js
+++ rv/ejemplofuncion.js
@@ -5,7 +5,7 @@ function init(p){
   escena.add(malla);
   
   camara= new THREE.PerspectiveCamera();
-  camara.position.z=5;
+  camara.position.z=10;
   camara.position.x=3.5;
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
