7941d77618821a5d92437b4bb1390c1ba4b96b69
AlfredoSM <ades_alfred@hotmail.com>
2016-09-23 22:38:24 -0500

    Update ejemplofuncion.js

--- rv/ejemplofuncion.js
+++ rv/ejemplofuncion.js
@@ -19,12 +19,10 @@ function init(p){
   if(sentido==0){
     malla.translateX(0.01);
     malla.rotateX(0.1);
-    malla.rotateZ(0.1);
   }
   else{
     malla.translateX(-0.01);
     malla.rotateX(-0.1);
-    malla.rotateZ(-0.1);
   }
   if(Math.abs(malla.position.x)>=7)
   sentido=1;
