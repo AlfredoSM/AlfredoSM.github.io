ba7fce8c48f460754bcbcad325e0d92f0311ed82
AlfredoSM <ades_alfred@hotmail.com>
2016-09-29 23:18:59 -0500

    Update tablerotextura.js

--- rv/tablerotextura.js
+++ rv/tablerotextura.js
@@ -98,7 +98,7 @@ TEXTURA.retrollamada3 = function( textura ){
 
 TEXTURA.retrollamada4 = function( textura ){
   var material = new THREE.MeshBasicMaterial( {map: textura} );
-	TEXTURA.tableron[(XX*8)+ZZ]= new THREE.Mesh( new THREE.BoxGeometry( 35, 1, 35), material );
+	TEXTURA.tableron= new THREE.Mesh( new THREE.BoxGeometry( 35, 1, 35), material );
 	TEXTURA.tableron.translateX(8*35);
 	TEXTURA.tableron.translateZ(8 * 35);
   TEXTURA.escena.add(TEXTURA.tablero);
