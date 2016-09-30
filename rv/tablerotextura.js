d3bb16cd8a112a749d83c324fb7d49246a21daf5
AlfredoSM <ades_alfred@hotmail.com>
2016-09-29 21:52:53 -0500

    Update tablerotextura.js

--- rv/tablerotextura.js
+++ rv/tablerotextura.js
@@ -69,7 +69,7 @@ TEXTURA.retrollamada = function( textura ){
   Torre.prototype= new THREE.Geometry();
 TEXTURA.Torre1 = new THREE.Mesh(new Torre(), material);
   TEXTURA.Torre3 = new THREE.Mesh(new Torre(), material);
-  TEXTURA.Torre3.translateY(35*7);
+  TEXTURA.Torre3.translateZ(35*7);
   TEXTURA.escena.add(TEXTURA.Torre1,TEXTURA.Torre3);
 }
 
@@ -80,23 +80,23 @@ TEXTURA.Torre2 = new THREE.Mesh(new Torre(), material);
   TEXTURA.Torre4 = new THREE.Mesh(new Torre(), material);
   TEXTURA.Torre2.translateX(35*7);
   TEXTURA.Torre4.translateX(35*7);
-  TEXTURA.Torre4.translateY(35*7);
+  TEXTURA.Torre4.translateZ(35*7);
   TEXTURA.escena.add(TEXTURA.Torre2,TEXTURA.Torre4);
 }
 
 TEXTURA.retrollamada3 = function( textura ){
   var material = new THREE.MeshBasicMaterial( {map: textura} );
- //for ( var XX = 0; XX < 8; XX ++ ){
-//for ( var ZZ = 0; ZZ < 8; ZZ ++ ){
+ for ( var XX = 0; XX < 8; XX ++ ){
+for ( var ZZ = 0; ZZ < 8; ZZ ++ ){
 	//if(((XX%2)&&(!(ZZ%2)))||((!(XX%2))&&(ZZ%2))){
-	TEXTURA.tablero = new THREE.Mesh( new THREE.BoxGeometry( 100, 1, 100), material );
-	//TEXTURA.tablero.translateX(XX*35);
-	//TEXTURA.tablero.translateZ(ZZ * 35);
+	TEXTURA.tablero [(XX*8)+ZZ]= new THREE.Mesh( new THREE.BoxGeometry( 100, 1, 100), material );
+	TEXTURA.tablero.translateX(XX*35);
+	TEXTURA.tablero.translateZ(ZZ * 35);
   TEXTURA.escena.add(TEXTURA.tablero);  
-	//}
+	}
   
-//}
-// }
+}
+}
 
 }
 
