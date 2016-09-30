e426bdbd8df0334d6425786ca0496630bffdf569
AlfredoSM <ades_alfred@hotmail.com>
2016-09-29 23:32:54 -0500

    Update tablerotextura.js

--- rv/tablerotextura.js
+++ rv/tablerotextura.js
@@ -124,7 +124,7 @@ TEXTURA.setup = function() {
   TEXTURA.camara = new THREE.PerspectiveCamera();
   TEXTURA.camara.position.z= 1000;
   TEXTURA.camara.position.x= 35*4;
-  TEXTURA.escena.rotateX(Math.PI/4);
+  //TEXTURA.escena.rotateX(Math.PI/4);
   TEXTURA.renderizador = new THREE.WebGLRenderer();
  TEXTURA.renderizador.setSize(600, 600);
  document.body.appendChild(TEXTURA.renderizador.domElement);
