7483aa77e7dda34cd353e9088df026ada0a3697a
AlfredoSM <ades_alfred@hotmail.com>
2016-10-06 15:22:08 -0500

    Update tablerotextura.js

--- rv/tablerotextura.js
+++ rv/tablerotextura.js
@@ -61,7 +61,7 @@ arbolForma.merge(baseAbajomalla.geometry, baseAbajomalla.matrix);
 arbolForma.merge(baseeMalla.geometry, baseeMalla.matrix);
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
-this = new THREE.Mesh(arbolForma, material)
+THREE.Mesh.call(this, arbolForma, material)
 
 }
 
