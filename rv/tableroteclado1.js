63906ef65e778ae4094f3ed06d2bdaefc207585f
AlfredoSM <ades_alfred@hotmail.com>
2016-11-22 15:15:29 -0600

    Update tableroteclado1.js

--- rv/tableroteclado1.js
+++ rv/tableroteclado1.js
@@ -231,7 +231,7 @@ TEXTURA.Torre1p.translateZ(60);
 
 	     else
 	aba=0;	
-	if(TEXTURA.Torre1p.position.x != TEXTURA.torre1.position.x && bandera==1;){
+	if(TEXTURA.Torre1p.position.x != TEXTURA.torre1.position.x && bandera===1){
 		TEXTURA.velocidadt1x=(TEXTURA.torre1.position.x-TEXTURA.Torre1p.position.x)/(TEXTURA.torre1.position.x-TEXTURA.Torre1p.position.x);
 		TEXTURA.velocidadt1z=(TEXTURA.torre1.position.z-TEXTURA.Torre1p.position.z)/(TEXTURA.torre1.position.z-TEXTURA.Torre1p.position.z);
 		TEXTURA.Torre1.translateX(TEXTURA.velocidadt1x);
