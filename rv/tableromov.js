1aa97a9de817e347ea9ed9539768107e4b3786b8
AlfredoSM <ades_alfred@hotmail.com>
2016-11-25 00:11:45 -0600

    Update tableromov.js

--- rv/tableromov.js
+++ rv/tableromov.js
@@ -318,7 +318,7 @@ TEXTURA.setup2 = function(){
 	setupDone = true;
 TEXTURA.torre1 = new Torre( TEXTURA.material3,TEXTURA.material7);
 	TEXTURA.torre1.translateY(25);
-  TEXTURA.escena.add(TEXTURA.torre1p);
+  TEXTURA.escena.add(TEXTURA.torre1);
   TEXTURA.tablero= new Tablero(TEXTURA.material1, TEXTURA.material2);
   TEXTURA.escena.add(TEXTURA.tablero);	 
   TEXTURA.camara = new THREE.PerspectiveCamera();
